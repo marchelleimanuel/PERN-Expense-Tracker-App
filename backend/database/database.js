ca51c55d17ebd850ed28deed2f04a0644a01b46b
Marchelle Imanuel <marchelleimanuel@gmail.com>
2025-07-30 12:32:33 +0700

    add validation for db prod

--- backend/database/database.js
+++ backend/database/database.js
@@ -3,10 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
-});
+// const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     logging: false,
+// });
+
+// export default db;
+
+const db = new Sequelize(
+    process.env.NODE_ENV === "production" ? process.env.DB_PROD_NAME : process.env.DB_NAME,
+    process.env.NODE_ENV === "production" ? process.env.DB_PROD_USER : process.env.DB_USER,
+    process.env.NODE_ENV === "production" ? process.env.DB_PROD_PASSWORD : process.env.DB_PASSWORD,
+    {
+        host: process.env.NODE_ENV === "production" ? process.env.DB_PROD_HOST : process.env.DB_HOST,
+        dialect: 'postgres',
+        logging: false,
+    }
+);
 
 export default db;
