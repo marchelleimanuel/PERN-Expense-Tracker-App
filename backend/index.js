13981f746e6cacc513a0d91200c997e44f9510a9
Marchelle Imanuel <marchelleimanuel@gmail.com>
2025-07-30 14:41:50 +0700

    add configure for cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,10 @@ dotenv.config();
 const app = express();
 const port = process.env.APP_PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: ['https://pern-expense-tracker-app.vercel.app/'],
+    credentials: true
+}));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello from backend!");
