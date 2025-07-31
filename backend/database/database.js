c38464e59c18b5ad62cf03229278defcff0ccdb6
Marchelle Imanuel <marchelleimanuel@gmail.com>
2025-07-31 12:07:24 +0700

    test

--- backend/database/database.js
+++ backend/database/database.js
@@ -11,10 +11,18 @@ dotenv.config();
 
 // export default db;
 
-const db = new Sequelize(process.env.DATABASE_URL, {
+const isProd = process.env.RAILWAY_ENVIRONMENT_NAME === "production";
+console.log('bener kok ', isProd);
+
+const db = new Sequelize(
+  isProd ? process.env.DB_PROD_NAME : process.env.DB_NAME,
+  isProd ? process.env.DB_PROD_USER : process.env.DB_USER,
+  isProd ? process.env.DB_PROD_PASSWORD : process.env.DB_PASSWORD,
+  {
+    host: isProd ? process.env.DB_PROD_HOST : process.env.DB_HOST,
     dialect: "postgres",
     logging: false,
-});
-
+  }
+);
 
 export default db;
